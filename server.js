8e38e2db2a4a966c2b6253b96236acacedde7897
Anshu Malaiya <anshumalaiyaiitr@gmail.com>
2024-12-26 18:35:35 +0530

    initial commit

--- server.js
+++ server.js
@@ -4,14 +4,27 @@ const dotenv = require("dotenv");
 const cors = require("cors");
 
 dotenv.config();
+
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+// MongoDB connection
+mongoose
+    .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log("Connected to MongoDB"))
+    .catch((err) => console.error("MongoDB connection error:", err));
 
-app.use("/auth", require("./routes/auth"));
-app.use("/buddies", require("./routes/buddies"));
-app.use("/groups", require("./routes/groups"));
+// Importing routes
+const authRoutes = require("./routes/auth");
+const buddiesRoutes = require("./routes/buddies");
+const groupsRoutes = require("./routes/groups");
 
-app.listen(3000, () => console.log("Server running on http://localhost:3000"));
+// Using routes
+app.use("/auth", authRoutes);
+app.use("/buddies", buddiesRoutes);
+app.use("/groups", groupsRoutes);
+
+// Start the server
+const PORT = 3000;
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
