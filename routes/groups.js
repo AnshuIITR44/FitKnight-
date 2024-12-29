ac41db0164f563c5d25331faa46f7ef652585d9a
AnshuIITR44 <anshumalaiyaiitr@gmail.com>
2024-12-29 16:26:17 +0530

    Update groups.js

--- routes/groups.js
+++ routes/groups.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Group = require("../models/Group");
+const Group = require("../models/group");
 
 router.post("/", async (req, res) => {
   try {
