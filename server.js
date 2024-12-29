da3d982d3b513f452a93b501513fe576571b55e7
Anshu Malaiya <anshumalaiyaiitr@gmail.com>
2024-12-29 15:32:47 +0530

    first commit

--- server.js
+++ server.js
@@ -3,28 +3,40 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const cors = require("cors");
 
-dotenv.config();
+dotenv.config(); // Load environment variables
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-// MongoDB connection
-mongoose
-    .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log("Connected to MongoDB"))
-    .catch((err) => console.error("MongoDB connection error:", err));
+// MongoDB connection using the .env file for the connection string
+mongoose.connect("mongodb+srv://anshumalaiyaiitr:AnshuIITR%40446@cluster0.i5kz9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", { connectTimeoutMS: 30000 })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("MongoDB connection error:", err));
 
-// Importing routes (relative paths)
-const authRoutes = require("./auth");
-const buddiesRoutes = require("./buddies");
-const groupsRoutes = require("./groups");
+// Importing route files (Ensure these files exist in the routes folder)
+const buddiesRoutes = require("./routes/buddies");
+const groupsRoutes = require("./routes/groups");
+const authRoutes = require("./routes/auth");
 
-// Using routes
-app.use("/auth", authRoutes);
+// Use routes
 app.use("/buddies", buddiesRoutes);
 app.use("/groups", groupsRoutes);
+app.use("/auth", authRoutes);
 
-// Start the server
-const PORT = 3000;
+// Handle 404 errors for undefined routes
+app.use((req, res, next) => {
+  const error = new Error('Not Found');
+  error.status = 404;
+  next(error);
+});
+
+// Generic error handler
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({ message: error.message });
+});
+
+// Start server
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
