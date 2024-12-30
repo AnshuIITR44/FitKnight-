8e2b6dbe38003edd481e31cc848556f39dea75ed
AnshuIITR44 <anshumalaiyaiitr@gmail.com>
2024-12-30 19:52:52 +0530

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ const authenticateToken = require("../middleware/authenticateToken");
 const User = require("../models/user");
 const router = express.Router();
 
-// Get user profile
+// Fetch user profile
 router.get("/profile", authenticateToken, async (req, res) => {
   try {
     const user = await User.findById(req.user.id);
@@ -23,25 +23,5 @@ router.get("/profile", authenticateToken, async (req, res) => {
     res.status(500).json({ error: "Internal server error." });
   }
 });
-
-// Update user profile
-router.get("/profile", authenticateToken, async (req, res) => {
-  try {
-    const user = await User.findById(req.user.id);
-    if (!user) {
-      return res.status(404).json({ error: "User not found." });
-    }
-    res.json({
-      username: user.username,
-      fitnessGoals: user.fitnessGoals,
-      preferences: user.preferences,
-      availability: user.availability,
-    });
-  } catch (error) {
-    console.error("Error fetching profile:", error);
-    res.status(500).json({ error: "Internal server error." });
-  }
-});
-
 
 module.exports = router;
