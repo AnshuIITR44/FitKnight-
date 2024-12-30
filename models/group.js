ca85008df303aa146c7bde4a1b27e55001f62263
AnshuIITR44 <anshumalaiyaiitr@gmail.com>
2024-12-30 18:57:49 +0530

    Add files via upload

--- models/group.js
+++ models/group.js
@@ -2,10 +2,7 @@ const mongoose = require("mongoose");
 
 const groupSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  activityType: { type: String, required: true },
-  location: { type: String, required: true },
-  schedule: { type: String, required: true },
-  members: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+  members: [{ type: mongoose.Schema.Types.ObjectId, ref: "Buddy" }],
 });
 
 module.exports = mongoose.model("Group", groupSchema);
