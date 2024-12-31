f089ffde34a3c846b5ed780d17d6c2d304154c98
AnshuIITR44 <anshumalaiyaiitr@gmail.com>
2024-12-31 16:08:15 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -3,10 +3,7 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  role: { type: String, enum: ["buddy", "organizer"], required: true },
-  profilePicture: { type: String, default: "" }, // Path to profile picture
-  about: { type: String, default: "" },
-  fitnessGoals: { type: String, default: "" },
+  role: { type: String, default: "user" },
 });
 
 module.exports = mongoose.model("User", userSchema);
