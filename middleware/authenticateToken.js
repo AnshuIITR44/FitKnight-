3523ab6da4552d586f5259648ba806c7e0cd0446
AnshuIITR44 <anshumalaiyaiitr@gmail.com>
2025-01-03 21:00:43 +0530

    Update authenticateToken.js

--- middleware/authenticateToken.js
+++ middleware/authenticateToken.js
@@ -4,16 +4,11 @@ function authenticateToken(req, res, next) {
   const authHeader = req.headers["authorization"];
   const token = authHeader && authHeader.split(" ")[1];
 
-  if (!token) {
-    return res.status(401).json({ success: false, message: "No token provided." });
-  }
+  if (!token) return res.status(401).json({ success: false, message: "Access Denied" });
 
   jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-    if (err) {
-      return res.status(403).json({ success: false, message: "Invalid token." });
-    }
-
-    req.user = user;
+    if (err) return res.status(403).json({ success: false, message: "Invalid Token" });
+    req.user = user; // Attach the decoded user info to the request
     next();
   });
 }
