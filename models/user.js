bd65d9f477afdf0637903bb47b9cb17ef720d967
AnshuIITR44 <anshumalaiyaiitr@gmail.com>
2025-01-07 14:08:45 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -6,6 +6,8 @@ const userSchema = new mongoose.Schema({
   profilePicture: { type: String, default: "default-profile.jpg" },
   role: { type: String, enum: ["buddy", "organizer"], required: true },
   fitnessGoals: { type: String, default: "Not set" },
+  workoutPreferences: { type: String, default: "Not set" },
+  availability: { type: String, default: "Not set" },
   about: { type: String, default: "Not set" },
   fitnessHistory: { type: [String], default: [] },
   phone: { type: String, default: "Not set" },
